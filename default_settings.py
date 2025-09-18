542109da78b9fdaa5365073b1dfc3c3aa761967a
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-09-18 10:27:22 +0200

    Explicitly defining api permissions

--- default_settings.py
+++ default_settings.py
@@ -215,7 +215,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
