499fca9001eb673d2b660e17bdedd2ac84d49c55
Stéphane Malta e Sousa <stephane.maltaesousa@ne.ch>
2026-03-13 10:30:57 +0100

    Fix tests sitn (#229)
    * Update documentation
    
    * Skip oidc if not used
    
    * Fix public/private metadata
    
    * Do not read german if not used
    
    * do not run fixturize
    
    * always test in english
    
    * default to language defined in settings
    
    * revert metadata count

--- api/tests/test_translations.py
+++ api/tests/test_translations.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.core import mail
-from rest_framework import status
+from django.test import override_settings
+from pathlib import Path
 from rest_framework.test import APITestCase
 from api.models import (
   Identity,
@@ -10,6 +11,8 @@ from api.models import (
 )
 from api.tests.factories import BaseObjectsFactory
 
+
+@override_settings(LANGUAGE_CODE='en')
 class EmailTranslationTest(APITestCase):
 
     def setUp(self):
@@ -50,8 +53,10 @@ class EmailTranslationTest(APITestCase):
         order.next_status_on_extract_input()
         self.assertEqual(mail.outbox[-1].subject, "Geoshop - Download ready", "English subject")
 
-        order.client.identity.language = Identity.PreferredLanguage.GERMAN
-        order.client.identity.save()
-        order.order_status = Order.OrderStatus.READY
-        order.next_status_on_extract_input()
-        self.assertEqual(mail.outbox[-1].subject, "Geoshop - Ihre Bestellung steht zum Download bereit.", "German subject")
+        german_locale_path = Path('../locale/de/LC_MESSAGES/django.mo')
+        if german_locale_path.is_file():
+            order.client.identity.language = Identity.PreferredLanguage.GERMAN
+            order.client.identity.save()
+            order.order_status = Order.OrderStatus.READY
+            order.next_status_on_extract_input()
+            self.assertEqual(mail.outbox[-1].subject, "Geoshop - Ihre Bestellung steht zum Download bereit.", "German subject")
