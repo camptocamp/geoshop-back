e7bde81700a1e0499e7dcffadfa0cb8acc52aedc
Dominik Frey <dominik.frey@camptocamp.com>
2026-09-16 11:20:05 +0200

    GSGGR-744 Enhance local development setup with Makefile targets, update product feeds, and introduce localhost Docker config

--- Makefile
+++ Makefile
@@ -5,6 +5,45 @@ export DOCKER_BUILDKIT=1
 
 .DEFAULT_GOAL := help
 
+.env:
+	cp .env.sample .env
+	sed -i 's/PGHOST=db/PGHOST=localhost/g' .env
+
+settings.py:
+	cp -vn default_settings.py settings.py
+
+.venv/touchfile: settings.py .env
+	python -m venv .venv
+	. .venv/bin/activate; \
+	pip install poetry; \
+	poetry install --no-root
+	touch .venv/touchfile
+
+venv: .venv/touchfile
+
+.PHONY: run-db-localhost
+run-db-localhost:
+	docker compose -f docker-compose.yml -f docker-compose.localhost.yml up -d db
+
+.PHONY: run-manage-scripts
+run-manage-scripts: venv run-db-localhost
+	. .venv/bin/activate; \
+	python manage.py migrate; \
+	python manage.py collectstatic --no-input; \
+	python manage.py compilemessages --locale=de; \
+	python manage.py fixturize; \
+	python manage.py seed
+
+.PHONY: run-server
+run-server: run-manage-scripts
+	. .venv/bin/activate; \
+	python manage.py runserver
+
+clean: docker-clean
+	rm -rf .venv
+	rm settings.py
+	rm .env
+
 .PHONY: build
 build: ## Build docker image
 	docker build --no-cache --tag=camptocamp/geoshop-api:$(VERSION) \
@@ -27,16 +66,20 @@ test: ## Run tests
 	docker compose exec -T api python manage.py test -v 2 --force-color --noinput
 
 .PHONY: prepare_env
-prepare_env: destroy_env ## Prepare Docker environment
+prepare_env: docker-down ## Prepare Docker environment
 	docker compose up --build -d
 	until [ "$$(docker inspect -f '{{.State.Health.Status}}' geoshop-back-api-1)" = "healthy" ]; do \
 		echo "Waiting for api..."; \
 		sleep 1; \
 	done;
 
-.PHONY: destroy_env
-destroy_env: ## Destroy Docker environment
+.PHONY: docker-down
+docker-down: ## Stop and remove docker containers
 	docker compose down --remove-orphans
+
+.PHONY: docker-clean
+docker-clean: ## Stop and remove docker containers, and remove volumes and images
+	docker compose down --remove-orphans -v --rmi='all'
 
 .PHONY: help
 help: ## Display this help
--- api/feed.py
+++ api/feed.py
@@ -1,26 +1,54 @@
 from django.contrib.syndication.views import Feed
+from django.http import HttpRequest
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
-from .models import ProductUpdate
+from .models import ProductUpdate, Product
+
 
 # check for naming (DatasetUpdateFeed) is a Dataset = Product?
 class OverallProductUpdateFeed(Feed):
+    title = "Product updates"
+    description = "Latest product update feed"
+
+    def link(self):
+        return reverse("product-update-feed")
 
     def items(self):
         return ProductUpdate.objects.order_by("-created_at")[:15]
 
     def item_title(self, item: ProductUpdate):
-        return item.product.metadata.name
+        return item.product.label
 
     def item_description(self, item: ProductUpdate):
-        return item.product.metadata.name + ": last data import at " + item.created_at.strftime("%d/%m/%Y %H:%M:%S")
+        return item.product.label + ": last data import at " + item.created_at.strftime("%d/%m/%Y %H:%M:%S")
 
-    # item_link is only needed if item has no get_absolute_url method.
     def item_link(self, item: ProductUpdate):
-        return reverse("product-update", args=[item.pk])
+        return reverse("single-product-update-feed", args=[item.product.id])
 
 
-class SingleProductUpdateFeed(OverallProductUpdateFeed):
+class SingleProductUpdateFeed(Feed):
 
-    def items(self):
-        return ProductUpdate.objects.filter(product__metadata_id=self.kwargs["pk"]).order_by("-created_at").first()
+    def get_object(self, request: HttpRequest, *args, **kwargs):
+        return get_object_or_404(ProductUpdate.objects.filter(product_id=kwargs["pk"]))
+
+    def title(self, item: ProductUpdate):
+        return item.product.label
+
+    def description(self, item: ProductUpdate):
+        return "Latest updates/imports for product " + item.product.label
+
+    def link(self, item: ProductUpdate):
+        return reverse("single-product-update-feed", args=[item.product.id])
+
+    def items(self, item: ProductUpdate):
+        return ProductUpdate.objects.filter(product_id=item.product.id).order_by("-created_at")
+
+    def item_title(self, item: ProductUpdate):
+        return item.product.label
+
+    def item_description(self, item: ProductUpdate):
+        return "Data set updated at " + item.created_at.strftime("%d/%m/%Y %H:%M:%S")
+
+    def item_link(self, item: ProductUpdate):
+        return reverse("single-product-update-feed", args=[item.product.id])
